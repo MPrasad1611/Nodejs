123e3f1300c798ca37af1888bcb52c836af481eb
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-25 18:05:05 +0530

    changes

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const app=require("./mod1")
-app.sample();
+const app=require("http");
+const server=app.createServer((req,res)=>{
+    res.write("hello world")
+    res.end("response Provided")
+})
+server.listen("3101",()=>{
+    console.log("it's Working")
+})
