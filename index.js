88d6612af9559ad9816f107dc04f61076dd7e79e
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-25 18:38:20 +0530

    changes

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server=app.createServer((req,res)=>{
     res.write("hello world")
     res.end("response Provided")
 })
-server.listen("3101","192.168.1.12",()=>{
+server.listen("3101",()=>{
     console.log("it's Working")
 })
