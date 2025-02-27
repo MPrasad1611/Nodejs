ea1bd2892a0b2bde4a2f4ede7908d0dd090d50da
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-27 13:02:24 +0530

    changes

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-const app = require("http");
-// const server=app.createServer((req,res)=>{
-//     res.write("hello world")
-//     res.end("response Provided")
-// })
-let obj = { name: "Prasad", city: "vizag" };
-const server = app.createServer((req, res) => {
-  if (req.method == "GET") {
-    if (req.url == "/home") {
-      res.writeHead(201, "responsive received", {
-        "content-type": "application/json",
-      });
-      res.write("Welcome to home page");
-      res.end();
-    } else if (req.url == "/about") {
-      res.writeHead(201, "responsive received", {
-        "content-type": "application/json",
-      });
-      res.write("Welcome to About page");
-      res.end();
-    } else {
-      res.writeHead(201, "responsive received", {
-        "content-type": "application/json",
-      });
-      res.write("Welcome to other page");
-      res.end();
-    }
-  } else {
-    res.end("invalid request");
-  }
-});
-server.listen("3101", () => {
-  console.log("it's Working");
-});
+const app=require("http");
+const server=app.createServer((req,res)=>{
+    res.write("hello world")
+    res.end("response Provided")
+})
+server.listen("3101",()=>{
+    console.log("it's Working")
+})
