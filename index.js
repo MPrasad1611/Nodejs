893245a32fa6587c65a57c0e0a0f2aed51fb158c
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-27 12:56:16 +0530

    changes

--- index.js
+++ index.js
@@ -6,11 +6,29 @@ const app = require("http");
 let obj = { name: "Prasad", city: "vizag" };
 const server = app.createServer((req, res) => {
   if (req.method == "GET") {
-    res.writeHead(201, "responsive received", {
-      "content-type": "application/json",
-    });
-    res.write(JSON.stringify(obj));
-    res.end();
+    if(req.url=="/home"){
+        res.writeHead(201, "responsive received", {
+            "content-type": "application/json",
+          });
+          res.write("Welcome to home page");
+          res.end();
+    }
+    else if(req.url=="/about") {
+        res.writeHead(201, "responsive received", {
+            "content-type": "application/json",
+          });
+          res.write("Welcome to About page");
+          res.end();
+
+    }
+    else{
+        res.writeHead(201, "responsive received", {
+            "content-type": "application/json",
+          });
+          res.write("Welcome to other page");
+          res.end();
+    }
+    
   }
   else{
      res.end("invalid request")
