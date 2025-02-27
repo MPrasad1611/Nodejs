db1297e4e7b15cf96ebc15e54903a77426655c13
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-27 12:59:28 +0530

    changes

--- index.js
+++ index.js
@@ -6,32 +6,27 @@ const app = require("http");
 let obj = { name: "Prasad", city: "vizag" };
 const server = app.createServer((req, res) => {
   if (req.method == "GET") {
-    if(req.url=="/home"){
-        res.writeHead(201, "responsive received", {
-            "content-type": "application/json",
-          });
-          res.write("Welcome to home page");
-          res.end();
+    if (req.url == "/home") {
+      res.writeHead(201, "responsive received", {
+        "content-type": "application/json",
+      });
+      res.write("Welcome to home page");
+      res.end();
+    } else if (req.url == "/about") {
+      res.writeHead(201, "responsive received", {
+        "content-type": "application/json",
+      });
+      res.write("Welcome to About page");
+      res.end();
+    } else {
+      res.writeHead(201, "responsive received", {
+        "content-type": "application/json",
+      });
+      res.write("Welcome to other page");
+      res.end();
     }
-    else if(req.url=="/about") {
-        res.writeHead(201, "responsive received", {
-            "content-type": "application/json",
-          });
-          res.write("Welcome to About page");
-          res.end();
-
-    }
-    else{
-        res.writeHead(201, "responsive received", {
-            "content-type": "application/json",
-          });
-          res.write("Welcome to other page");
-          res.end();
-    }
-    
-  }
-  else{
-     res.end("invalid request")
+  } else {
+    res.end("invalid request");
   }
 });
 server.listen("3101", () => {
