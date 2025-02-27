06666d553e0cc2b78c423ca9214d061fd0cbf877
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-27 12:37:50 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const app=require("http");
-const server=app.createServer((req,res)=>{
-    res.write("hello world")
-    res.end("response Provided")
-})
-server.listen("3101",()=>{
-    console.log("it's Working")
-})
+const app = require("http");
+// const server=app.createServer((req,res)=>{
+//     res.write("hello world")
+//     res.end("response Provided")
+// })
+let obj = { name: "Prasad", city: "vizag" };
+const server = app.createServer((req, res) => {
+  res.writeHead(201, "responsive received", {
+    "content-type": "application/json",
+  });
+  res.write(JSON.stringify(obj));
+  res.end();
+});
+server.listen("3101", () => {
+  console.log("it's Working");
+});
